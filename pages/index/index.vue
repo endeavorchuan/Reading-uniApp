fb869065c498e650bb7068b27bddbaf4ac7b22b6
川 <chc82347794@163.com>
2022-05-26 11:18:10 +0800

    项目初始化 ----- 结构搭建

---
+++ pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<view class="container">
+		<text>left</text>
+		<text>center</text>
+		<text>right</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style scoped lang="scss">
+.container {
+	@include flex(flex-start, column);
+	color: $base-color;
+}
+</style>
